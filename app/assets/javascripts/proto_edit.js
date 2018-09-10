e448d4f5cec38677185ed8c5b2c15ffc1eb84839
y-kazuya <y.kazuya.0124@au.com>
2018-09-10 14:22:46 +0900

    Commit prot
    プロトタイプ編集機能を実装しました
    目的：プロトタイプを編集できたら間違いとかあった時便利よね
    
    何をしたか：
    ・rootでprototypeコントローラーのeditとupdateを追加した
    ・prototypeコントロローラーでeditにはprototypeの情報を入れ
       updateにはeditから送られてきた情報をupdateさせる。
       その時のparamsはcaputuded_imageのidが必要だっため、専用のメソッドを追　　　　　　
    　加
    　成功したらトップに戻りアラート表示
    
    　・editのviewページを追加
        ・editのjsファイルを追加
    
    JSファイル内での処理
      ・メイン、サブ画像をプレビュー表示
       ・すでに投稿されているサブ画像の数を取得して、画像投稿フォームを差し込む数を決めている。
    　・画像投稿フォーム、画像投稿フォーム追加ボタンの差し込み
        ・画像投稿フォーム追加ボタンを押したときの、処理（画像投稿フォームの表示）
    
    ##コード汚くてすいません！！

---
+++ app/assets/javascripts/proto_edit.js
@@ -0,0 +1,93 @@
+$(function(){
+  // main_image_preview/////////////////
+  $("#main_image_uploader").find("input").on("change", function(e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+
+    if(file.type.indexOf("image") != 0){
+      alert("画像ファイルを選択してください");
+      return false;
+    }
+
+    reader.onload = (function(){
+      return function(e){
+        $("#main_image").attr("src", e.target.result);
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  })
+
+  // sub_image_preview///////////////////////
+  $('.list-group').on("click", $(this).find(".list-group-item"), function() {
+    $("#sub_image_uploder").find("input").on("change", function(e) {
+      var file = e.target.files[0]
+      var reader = new FileReader();
+      var target = $(this).siblings("#sub_image");
+
+      if(file.type.indexOf("image") != 0){
+        alert("画像ファイルを選択してください");
+        return false;
+      }
+
+      reader.onload = (function(){
+        return function(e){
+          target.attr("src", e.target.result);
+        };
+      })(file);
+      reader.readAsDataURL(file);
+    })
+  })
+
+  //add_picture
+  function buildHtml(i){
+    var html = `<li class="list-group-item col-md-4 new_sub edit_subs" style="display:none;">
+        <div class="image-upload">
+          <img style="width: 100%; height: 100%;" id="sub_image" src="">
+          <input type="file" name="prototype[captured_images_attributes][${i}][content]" id="prototype_captured_images_attributes_${i}_content">
+          <input value="sub" type="hidden" name="prototype[captured_images_attributes][${i}][status]" id="prototype_captured_images_attributes_${i}_status">
+        </div>
+      </li>`
+
+    return html;
+  }
+
+  function buildAddSub(){
+    var add_sub  = `<li class="list-group-item col-md-4" id="add_sub" >
+        <div>
+          追加したいな
+        </div>
+      </li>`
+
+    return add_sub;
+  }
+  if (location.pathname.match(/\/prototypes\/\d+\/edit/)){
+    var sub_count = $(".edit_subs").length
+    var need_new = 3 - sub_count
+    if (sub_count == 0){
+      var num = 1
+    }
+    else{
+      var get_num = $(".edit_subs").find("input").attr("name").match(/\d/)
+      var num = Number(get_num) + sub_count
+    }
+    if(need_new != 0){
+      var target = $(".proto-sub-list")
+      for (i = sub_count; i < need_new + sub_count ; i = i +1){
+        var html = buildHtml(num)
+        num += 1
+        target.append(html);
+      }
+      var add_sub = buildAddSub()
+      target.append(add_sub)
+    }
+  }
+
+  $("body").on("click" ,$("#add_sub"), function(){
+    var target = $(".edit_subs")[sub_count];
+    $(target).css("display", "block");
+    sub_count += 1;
+    if(sub_count == 3){
+      $("#add_sub").css("display", "none");
+    }
+  })
+})
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :show, :destroy] do
+  resources :prototypes do
     resources :captured_images, only: [:create, :update, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
