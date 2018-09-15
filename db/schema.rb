69a8b4b9319eac7894aa9a43dc225473b283bd32
MoritaDaichi <kireazi19990424@gmail.com>
2018-09-15 12:55:37 +0900

    delete

--- db/schema.rb
+++ db/schema.rb
@@ -32,29 +32,6 @@ ActiveRecord::Schema.define(version: 20160405022520) do
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255,   default: "", null: false
-    t.string   "encrypted_password",     limit: 255,   default: "", null: false
-    t.string   "reset_password_token",   limit: 255
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,     default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "name",                   limit: 255
-    t.text     "profile",                limit: 65535
-    t.string   "position",               limit: 255
-    t.text     "occupation",             limit: 65535
-    t.string   "avatar",                 limit: 255
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
   add_foreign_key "captured_images", "prototypes"
   add_foreign_key "prototypes", "users"
 end
