5883f8b4ce0aed9fd6eea64edadb76cc3e18db3d
y-kazuya <y.kazuya.0124@au.com>
2018-09-22 09:47:11 +0900

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	app/views/prototypes/index.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     resources :captured_images, only: [:create, :update, :destroy]
     resources :comments , only: [:create, :edit, :update, :destroy]
   end
+  resources :tags, only: [:index,:show] # id: '/^[a-z]+$/'idを拡張してurlにタグ名で表示
   resources :users, only: [:show, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
+ActiveRecord::Schema.define(version: 20180921111044) do
+
 ActiveRecord::Schema.define(version: 20180915074942) do
 
   create_table "captured_images", force: :cascade do |t|
@@ -43,6 +46,20 @@ ActiveRecord::Schema.define(version: 20180915074942) do
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
 
+  create_table "prototypes_tags", force: :cascade do |t|
+    t.integer "prototype_id", limit: 4
+    t.integer "tag_id",       limit: 4
+  end
+
+  add_index "prototypes_tags", ["prototype_id"], name: "index_prototypes_tags_on_prototype_id", using: :btree
+  add_index "prototypes_tags", ["tag_id"], name: "index_prototypes_tags_on_tag_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
     t.string   "encrypted_password",     limit: 255,   default: "", null: false
@@ -70,4 +87,6 @@ ActiveRecord::Schema.define(version: 20180915074942) do
   add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
   add_foreign_key "prototypes", "users"
+  add_foreign_key "prototypes_tags", "prototypes"
+  add_foreign_key "prototypes_tags", "tags"
 end
