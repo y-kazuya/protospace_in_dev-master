613412a2cfa3ded38aa66de73818b1e5642b29c9
y-kazuya <y.kazuya.0124@au.com>
2018-09-22 10:11:52 +0900

    Rer
    rere

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180921111044) do
-
-ActiveRecord::Schema.define(version: 20180915074942) do
 
   create_table "captured_images", force: :cascade do |t|
     t.string  "content",      limit: 255
